2ee0e635eff85008361e0862b61628bd824ca74c
Rohit Choukiker <rohitchoukiker2803@gmail.com>
2025-06-21 15:42:39 +0530

    learn query parameter

--- main.py
+++ main.py
@@ -25,6 +25,26 @@ def read_item(item_id: int):
 def read_item_details(item_id: int):
     return {"item_id": item_id, "details": "This is a detailed view of the item."}
 
+
+# Blog endpoint with a query parameter
+@app.get("/blog")
+def read_blog(limit)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
 
     uvicorn.run(app, host="127.0`0.1", port=8000, log_level="info")
