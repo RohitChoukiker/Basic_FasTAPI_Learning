07be8afe0f29d01490603958d636443038b0e145
Rohit Choukiker <rohitchoukiker2803@gmail.com>
2025-06-21 15:35:29 +0530

    learning dynamic routing

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import uvicorn
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+   
+    price: float
+    
+@app.post("/items/")
+def create_item(item: Item):
+    return {"item": item}
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
 
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/items/{item_id}/details")
+def read_item_details(item_id: int):
+    return {"item_id": item_id, "details": "This is a detailed view of the item."}
 
 if __name__ == "__main__":
 
